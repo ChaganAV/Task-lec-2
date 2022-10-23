b8cb6f5eb3a547d329bef3907b1c617301309a2d
Alexandr <jwchaganav@gmail.com>
2022-10-23 19:56:25 +0400

    функция GetArraySort

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Функция упорядочивания массива
+Console.WriteLine("Введите массив чисел за одни раз: ");
+
+string? strArray = Console.ReadLine();
+
+int[] GetArrayDigit(string arrayDigit)
+{
+    int[] arrNum = new int[arrayDigit.Length];
+    for(int i = 0; i < arrayDigit.Length; i++)
+    {
+
+    }
+    return arrNum;
+}
+
+int[] GetArraySort(int[] array)
+{
+    int[] arrNew = new int[array.Length];
+    arrNew[0] = array[0];
+    int temp = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        for(int j = 1; j < array.Length-1; j++)
+        {
+            if (arrNew[i] < array[j])
+                arrNew[j] = array[j];
+            else
+            {
+                temp = arrNew[i];
+                arrNew[i] = array[j];
+                arrNew[j] = temp;
+            }       
+        }
+    }
+
+    return arrNew;
+}
